75ca012dedfafe00d827ceca952882b9baac46bf
thousandbeach <thousandbeach.td@gmail.com>
2018-05-24 03:05:53 +0900

    register.blade.php のAuthとデザイン

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
+<!--
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -74,4 +75,65 @@
         </div>
     </div>
 </div>
+-->
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <div class="card p-4">
+                <div class="card-header text-center text-uppercase h4 font-weight-light">
+                    サインアップ
+                </div>
+
+                <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    {{ csrf_field() }}
+
+                <div class="card-body py-5">
+                    <div class="form-group">
+                        <label class="form-control-label">お名前</label>
+                        <input type="name" name="name" class="form-control{{ $errors->has('name') ? ' has-error text-danger' : '' }}" value="{{ old('name') }}" required autofocus>
+                        @if ($errors->has('name'))
+                            <span class="help-block">
+                                <strong class="text-danger">{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-control-label">メールアドレス</label>
+                        <input type="email" class="form-control{{ $errors->has('email') ? ' has-error text-danger' : '' }}" name="email" value="{{ old('email') }}" required>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong class="text-danger">{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-control-label">パスワード</label>
+                        <input type="password" class="form-control{{ $errors->has('password') ? ' has-error text-danger' : '' }}" name="password" required>
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                <strong class="text-danger">{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-control-label">パスワード確認</label>
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-success btn-block">新規 登録</button>
+                </div>
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
