0d17227b0fd225f7d3116fa750baac6f05476412
thousandbeach <thousandbeach.td@gmail.com>
2018-05-24 18:42:10 +0900

    PostSeeder.phpのrunメソッドのテーブル名が間違っていたため、修正 sがついていなかった。正しくはposts DB::table('posts')->insert([

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -13,7 +13,7 @@ class PostSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('post')->insert([
+        DB::table('posts')->insert([
             ['user_id' => 1, 'title' => "Post One", 'content' => "Post one Content"],
             ['user_id' => 2, 'title' => "Post Two", 'content' => "Post two Content"],
             ['user_id' => 3, 'title' => "Post Three", 'content' => "Post three Content"],
