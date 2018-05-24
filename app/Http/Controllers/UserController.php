c1a91f2d76cdf2e2abe713f56d5c1ab60124a7de
thousandbeach <thousandbeach.td@gmail.com>
2018-05-25 06:15:53 +0900

    53 php artisan make:request UserUpdateを実行した

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,4 +15,21 @@ class UserController extends Controller
     public function comments(){
         return view('user.comments');
     }
+
+    // 管理パネルのprofileに相当
+    public function profile(){
+        return view('user.profile');
+    }
+
+    // 管理パネルのprofileの処理に相当
+    public function profilePost(Request $request){
+
+        $user = Auth::user();
+
+        $user->name = $request['name'];
+        $user->email = $request['email'];
+        $user->save();
+
+        return back();
+    }
 }
---
+++ resources/views/user/profile.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header bg-light">
+                            アカウント設定
+                        </div>
+
+                        <form action="{{ route('userProfilePost') }}" method="POST">
+                            {{ csrf_field() }}
+                        <div class="card-body">
+                            <div class="row mb-5">
+                                <div class="col-md-4 mb-4">
+                                    <div>プロフィール情報</div>
+                                    <div class="text-muted small">These information are visible to the public.</div>
+                                </div>
+
+                                <div class="col-md-8">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label class="form-control-label" for="name">お名前</label>
+                                                <input name="name" class="form-control" value="{{ Auth::user()->name }}">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label class="form-control-label" for="email">メールアドレス</label>
+                                                <input name="email" class="form-control" value="{{ Auth::user()->email }}">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <hr>
+
+                            <div class="row mt-5">
+                                <div class="col-md-4 mb-4">
+                                    <div>パスワード変更</div>
+                                    <div class="text-muted small">Leave credentials fields empty if you don't wish to change the password.</div>
+                                </div>
+
+                                <div class="col-md-8">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label class="form-control-label" for="password">現在のパスワード</label>
+                                                <input name="password" type="password" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label class="form-control-label" for="new_password">新しいパスワード</label>
+                                                <input name="new_password" type="password" class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label class="form-control-label" for="new_password_confirmation">新しいパスワード確認</label>
+                                                <input name="new_password_confirmation" type="password" class="form-control">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card-footer bg-light text-right">
+                            <button type="submit" class="btn btn-primary">設定を保存する</button>
+                        </div>
+
+                    </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
