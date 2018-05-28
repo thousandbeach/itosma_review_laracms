b9788b3f327b59c16ba6bbc87974654415cca000
thousandbeach <thousandbeach.td@gmail.com>
2018-05-28 23:49:24 +0900

    62 Admin Dashboard とuser/dashboard.blade.php の日本語化

--- resources/views/author/dashboard.blade.php
+++ resources/views/author/dashboard.blade.php
@@ -67,7 +67,7 @@
                 </div>
             </div>
 
-            <div class="row ">
+            <div class="row">
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -14,7 +14,7 @@
                         <div class="card-body d-flex justify-content-between align-items-center">
                             <div>
                                 <span class="h4 d-block font-weight-normal mb-2">{{ Auth::user()->comments->count() }}</span>
-                                <span class="font-weight-light">Comments</span>
+                                <span class="font-weight-light">Comments<br>(コメントの合計)</span>
                             </div>
 
                             <div class="h2 text-muted">
@@ -29,7 +29,7 @@
                         <div class="card-body d-flex justify-content-between align-items-center">
                             <div>
                                 <span class="h4 d-block font-weight-normal mb-2">{{ Auth::user()->comments->unique('post_id')->count() }}</span>
-                                <span class="font-weight-light">Commented posts</span>
+                                <span class="font-weight-light">Commented posts<br>(記事に対してコメントした人数)</span>
                             </div>
 
                             <div class="h2 text-muted">
@@ -40,7 +40,7 @@
                 </div>
             </div>
 
-            <div class="row ">
+            <div class="row">
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
