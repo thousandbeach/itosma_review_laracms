43d27d8ef7fe20d80a91becc56babcf68ae1fa74
thousandbeach <thousandbeach.td@gmail.com>
2018-06-17 16:30:06 +0900

    contact.blade.phpのプレースホルダを日本語に変更し、label for=もつけた。

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -27,36 +27,36 @@
           <form name="sentMessage" id="contactForm" novalidate>
             <div class="control-group">
               <div class="form-group floating-label-form-group controls">
-                <label>Name</label>
-                <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
+                <label for="name">お名前</label>
+                <input type="text" class="form-control" placeholder="お名前" id="name" required data-validation-required-message="Please enter your name.">
                 <p class="help-block text-danger"></p>
               </div>
             </div>
             <div class="control-group">
               <div class="form-group floating-label-form-group controls">
-                <label>Email Address</label>
-                <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
+                <label for="email">メールアドレス</label>
+                <input type="email" class="form-control" placeholder="メールアドレス" id="email" required data-validation-required-message="Please enter your email address.">
                 <p class="help-block text-danger"></p>
               </div>
             </div>
-            <div class="control-group">
+            <!--<div class="control-group">
               <div class="form-group col-xs-12 floating-label-form-group controls">
                 <label>Phone Number</label>
                 <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number.">
                 <p class="help-block text-danger"></p>
               </div>
-            </div>
+          </div>-->
             <div class="control-group">
               <div class="form-group floating-label-form-group controls">
-                <label>Message</label>
-                <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                <label for="message">メッセージ</label>
+                <textarea rows="5" class="form-control" placeholder="メッセージ" id="message" required data-validation-required-message="Please enter a message."></textarea>
                 <p class="help-block text-danger"></p>
               </div>
             </div>
             <br>
             <div id="success"></div>
             <div class="form-group">
-              <button type="submit" class="btn btn-primary" id="sendMessageButton">Send</button>
+              <button type="submit" class="btn btn-primary" id="sendMessageButton">送 信</button>
             </div>
           </form>
         </div>
